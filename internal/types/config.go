637553961d23d56d2748b1c2fed458f0e3c9bc96
TBroz15 <justtriple@outlook.com>
2025-09-04 15:38:05 +0800

    chore: wip
    remove option to whether or not to optimize assets
    
    if they do not want to optimize specific part of their packs, then the users must not use tesserpack in the first place
    
    like cmon

--- internal/types/config.go
+++ internal/types/config.go
@@ -1,34 +1,26 @@
 package types
 
 type JSONConfig struct {
-	Minify bool	`json:"minify"`
 	Strict bool	`json:"strict"`
-	Cache  bool	`json:"cache"`
-}
-
-type LANGConfig struct {
-	Minify bool	`json:"minify"`
-	Cache  bool	`json:"cache"`
 }
 
 type PNGConfig struct {
-	Optimize 	  bool `json:"optimize"`
 	Progressive   bool `json:"progressive"`
 	CompressLevel byte `json:"compressLevel"`
 	Quality		  byte `json:"quality"`
 }
 
 type JPGConfig struct {
-	Optimize 	  bool `json:"optimize"`
 	Progressive   bool `json:"progressive"`
 	Quality		  byte `json:"quality"`
 }
 
 type CompilerConfig struct {
 	JSON JSONConfig `json:"json"`
-	LANG LANGConfig `json:"lang"`
 	PNG  PNGConfig  `json:"png"`
 	JPG  JPGConfig  `json:"jpg"`
+
+	Cache bool `json:"cache"`
 }
 
 type IgnoreConfig struct {
